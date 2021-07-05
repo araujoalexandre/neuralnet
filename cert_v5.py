7db8e5573bf749d7bb2a2bab5205d69cf9cf2b39
Alexandre Araujo <aaraujo001@gmail.com>
2021-07-05 12:38:36 +0200

    update cert

--- cert_v5.py
+++ cert_v5.py
@@ -26,11 +26,11 @@ class Certificate:
 
   def __init__(self, sigma0, sigma1, p1, p2):
 
-    self.sigma0 = sigma0
-    self.sigma1 = sigma1
+    # self.sigma0 = sigma0
+    # self.sigma1 = sigma1
+    # self.dim = 10
     self.pABar_sigma0 = p1
     self.pABar_sigma1 = p2
-    self.dim = 10
     self.sample = 100000
     self.niter = 12
     if torch.cuda.is_available():
@@ -40,8 +40,8 @@ class Certificate:
     self.close_below_tol = 0.0001
     self.binary_search_tol = 0.0001
 
-    self.cohen_cert = self.sigma0 * sp_norm.ppf(self.pABar_sigma0)
-    print('cert cohen {:.4f}'.format(self.cohen_cert))
+    # self.cohen_cert = self.sigma0 * sp_norm.ppf(self.pABar_sigma0)
+    # print('cert cohen {:.4f}'.format(self.cohen_cert))
 
   def define_delta(self, dim, eps):
     delta = torch.zeros(dim, device=self.device)
@@ -66,32 +66,32 @@ class Certificate:
       break
     return start
 
-  def X1_term(self, noise, eps):
+  def X1_term(self, noise, eps, sigma0):
     dim = noise.shape[-1]
     delta = self.define_delta(dim, eps)
-    return -torch.norm(noise + delta, p=2, dim=1)**2 / (2 * self.sigma0**2)
+    return -torch.norm(noise + delta, p=2, dim=1)**2 / (2 * sigma0**2)
 
-  def Y1_term(self, noise, cst=None):
+  def Y1_term(self, noise, sigma0, constant=None):
     sample = len(noise)
-    if cst == None:
-      cst = torch.zeros(sample, device=self.device)
-    return -torch.norm(noise, p=2, dim=1)**2 / (2 * self.sigma0**2) - cst
+    if constant == None:
+      constant = torch.zeros(sample, device=self.device)
+    return -torch.norm(noise, p=2, dim=1)**2 / (2 * sigma0**2) - constant
 
-  def Y2_term(self, noise, log_k2, cst=None):
+  def Y2_term(self, noise, sigma1, log_k2, constant=None):
     sample = len(noise)
-    if cst == None:
-      cst = torch.zeros(sample, device=self.device)
-    return -torch.norm(noise, p=2, dim=1)**2 / (2 * self.sigma1**2) + log_k2 - cst
+    if constant == None:
+      constant = torch.zeros(sample, device=self.device)
+    return -torch.norm(noise, p=2, dim=1)**2 / (2 * sigma1**2) + log_k2 - constant
 
-  def compute_set(self, noise, eps, k1, log_k2, return_mean=True):
+  def compute_set(self, noise, eps, k1, log_k2, sigma0, sigma1, return_mean=True):
 
-    X1 = self.X1_term(noise, eps)
-    Y1 = self.Y1_term(noise).reshape(-1, 1)
-    Y2 = self.Y2_term(noise, log_k2).reshape(-1, 1)
+    X1 = self.X1_term(noise, eps, sigma0)
+    Y1 = self.Y1_term(noise, sigma0).reshape(-1, 1)
+    Y2 = self.Y2_term(noise, sigma1, log_k2).reshape(-1, 1)
 
     cst = torch.max(torch.cat([Y1, Y2], dim=1), dim=1)[0]
-    Y1 = self.Y1_term(noise, cst)
-    Y2 = self.Y2_term(noise, log_k2, cst)
+    Y1 = self.Y1_term(noise, sigma0, constant=cst)
+    Y2 = self.Y2_term(noise, sigma1, log_k2, constant=cst)
 
     exp_Y1 = torch.exp(Y1)
     exp_Y2 = torch.exp(Y2)
@@ -101,139 +101,77 @@ class Certificate:
     else:
       return X1 <= cst + torch.log( k1 * exp_Y1 + exp_Y2 )
 
-  def compute_k1_cohen(self, eps):
+  def compute_k1_cohen(self, eps, sigma0):
     """ Compute the constant k1 in the context of Cohen certificate """
-    k1 = np.exp((eps / self.sigma0 * sp_norm.ppf(self.pABar_sigma0) ) - eps**2 / (2 * self.sigma0 **2))
+    k1 = np.exp((eps / sigma0 * sp_norm.ppf(self.pABar_sigma0) ) - eps**2 / (2 * sigma0 **2))
     return k1
 
-  def compute_log_k(self, sigma):
+  def compute_log_k(self, sigma, dim):
     """ Compute the constant k """ 
-    dim = self.dim
     log_k = -(dim-2) * np.log(sigma) - ((dim-2)/2) * np.log(2) - loggamma((dim-1)/2) + 1/2 * np.log(pi)
     return log_k
 
-  def integrand(self, x, eps, k1, log_k2, sigma=0.25):
-    log_k = self.compute_log_k(sigma)
+  def integrand(self, x, dim, sigma0, sigma1, eps, k1, log_k2):
+    log_k = self.compute_log_k(sigma0, dim)
     x = torch.DoubleTensor(x)
     if self.device == 'cuda':
       x = x.cuda()
     x[:, 0] = torch.abs(x[:, 0])
-    exp = 1/(pi*sigma**2) * torch.exp(-torch.norm(x, p=2, dim=1)**2 / (2*sigma**2))
-    g = self.compute_set(x, eps, k1, log_k2, return_mean=False)
+    exp = 1/(pi*sigma0**2) * torch.exp(-torch.norm(x, p=2, dim=1)**2 / (2*sigma0**2))
+    g = self.compute_set(x, eps, k1, log_k2, sigma0, sigma1, return_mean=False)
     r = x[:, 0]
-    integral = exp * (r * np.exp(1/(self.dim-2) * log_k) * (g * 1.))**(self.dim-2)
+    integral = exp * (r * np.exp(1/(dim-2) * log_k) * (g * 1.))**(dim-2)
     integral = torch.nan_to_num(integral)
     return np.double(integral.detach().cpu().numpy())
 
-  def compute_from_integral(self, sigma, eps, k1, log_k2):
-
-    dim = self.dim
-
-    center = sigma * np.sqrt(self.dim - 2)
-    t = np.array([center, 0]).reshape(1, 2)
-    mode = self.integrand(t, eps, k1, log_k2, sigma=self.sigma0)
-    print(f'mode of curve: {center:.4f}, {mode:.4f}')
-
-    # a = 1/max_
-    # b = -center
-    # a = 1.
-    # b = 0.
-    # print(f'a = {a}, b = {b}')
-
-    # bound = lambda r: a * r + b 
-    # b1 = bound(center - 0.75)
-    # b2 = bound(center + 0.75)
-    # support = [b1, b2]
-    support = [center - 0.75, center + 0.75]
+  def compute_integral(self, dim, sigma0, sigma1, eps, k1, log_k2, support_r):
 
     @vegas.batchintegrand
-    def f1_vegas(x):
-      return self.integrand(
-        x, eps, k1, log_k2, sigma=sigma)
+    def integrand_vegas(x):
+      return self.integrand(x, dim, sigma0, sigma1, eps, k1, log_k2)
 
-    def compute_integral_from_support(f, x1, x2):
-      integ = vegas.Integrator([x1, x2])
-      integ(f, nitn=self.niter, neval=self.sample)
-      result = integ(f, nitn=self.niter, neval=self.sample)
-      return result
-
-    # int1 = norm1**(2-dim) * compute_integral_from_support(f1, [0, 1], [-100, 100])
-    # int2 = (2-dim) * np.log(norm2) + np.log(compute_integral_from_support(f2, [1, 12], [-100, 100]))
-    # return (int1 + np.exp(int2)).mean
-
-    # int1 = norm1**(2-dim) * compute_integral_from_support(f1, [0, 10], [-100, 100])
-
-    # print(norm1**(2-dim))
-
-    print(f'dim: {self.dim}, support {support}')
-    int1 = compute_integral_from_support(f1_vegas, support, [-2, 2])
-    # int1 = norm1**(2-dim) * compute_integral_from_support(f1_vegas, support, [-2, 2])
-    # int1 = (2-dim) * np.log(norm1) + np.log(compute_integral_from_support(f1_vegas, support, [-2, 2]))
-    # int1 = np.exp(int1)
-    print(f'p1 = {int1.mean:.5f}')
+    integ = vegas.Integrator([support_r, [-2, 2]])
+    integ(integrand_vegas, nitn=self.niter, neval=self.sample)
+    result = integ(integrand_vegas, nitn=self.niter, neval=self.sample)
+    return result
   
-    # print(f'dim: {self.dim}, support [2, 4]')
-    # int1 = norm1**(2-dim) * compute_integral_from_support(f1_vegas, [2, 4], [-2, 2])
-    # print(f'p1 = {int1.mean:.5f}')
-
-    return None
 
 
-  def find_mode_and_width_of_bell_curve(self, sigma, eps, k1, log_k2):
+  def find_mode_and_width_of_bell_curve(self, eps, k1, log_k2, sigma0, sigma1):
 
     print()
     points = 5000
 
-    # for i, dim in enumerate(np.arange(5, 60)):
-    for i, dim in enumerate([10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 150, 200]):
+    for i, dim in enumerate([5, 6, 7, 8, 9, 10, 15, 20, 30, 40, 50, 60, 70, 80, 90, 100, 150, 200]):
+    # for i, dim in enumerate([40]):
 
-      center = sigma * np.sqrt(dim - 2)
-      t = np.array([center, 0]).reshape(1, 2)
-      mode = self.integrand(t, eps, k1, log_k2, sigma=self.sigma0)
+      center = sigma0 * np.sqrt(dim - 2)
+      x = np.array([center, 0]).reshape(1, 2)
+      support = [center - 0.75, center + 0.75]
+      mode = self.integrand(x, dim, sigma0, sigma1, eps, k1, log_k2)
 
-      # r = np.linspace(np.maximum(center - 2, 0), center + 2, num=points)
-      # u = np.zeros(points)
-      # x = np.hstack((r.reshape(-1, 1), u.reshape(-1, 1))).reshape(points, 2)
-      # values = self.integrand(x, eps, k1, log_k2, sigma=sigma)
-
-      # min_support = np.maximum(r[values > bound].min(), 0)
-      # max_support = r[values > bound].max()
-      # width_support = max_support - min_support
-
-      @vegas.batchintegrand
-      def f1_vegas(x):
-        return self.integrand(
-          x, eps, k1, log_k2, sigma=sigma)
-
-      def compute_integral_from_support(f, x1, x2):
-        integ = vegas.Integrator([x1, x2])
-        integ(f, nitn=self.niter, neval=self.sample)
-        result = integ(f, nitn=self.niter, neval=self.sample)
-        return result
-
-      support = [np.maximum(center - 0.75, 0), center + 0.75]
-
-      # we increase the support if necessary 
-      p1 = 0.
-      while p1 < 0.69:
-        p1 = compute_integral_from_support(f1_vegas, support, [-2, 2])
-        support[0] -= 0.01
-        support[1] += 0.01
- 
-      # print('support big enough')
- 
-      while True:
-        # print(support, p1)
-        p1 = compute_integral_from_support(f1_vegas, support, [-2, 2])
-        if p1 < 0.698:
-          support[0] -= 0.01
-          support[1] += 0.01
-          p1 = compute_integral_from_support(f1_vegas, support, [-2, 2])
-          break
-        support[0] += 0.01
-        support[1] -= 0.01
-
+      support = [np.maximum(center - 0.55, 0), center + 0.55]
       width_support = support[1] - support[0]
+      p1 = self.compute_integral(dim, sigma0, sigma1, eps, k1, log_k2, support)
+
+      # # we increase the support if necessary 
+      # p1 = 0.
+      # while p1 < 0.69:
+      #   p1 = self.compute_integral(dim, sigma0, sigma1, eps, k1, log_k2, support)
+      #   support[0] -= 0.01
+      #   support[1] += 0.01
+      #
+      # while True:
+      #   p1 = self.compute_integral(dim, sigma0, sigma1, eps, k1, log_k2, support)
+      #   if p1 < 0.698:
+      #     support[0] -= 0.01
+      #     support[1] += 0.01
+      #     p1 = self.compute_integral(dim, sigma0, sigma1, eps, k1, log_k2, support)
+      #     break
+      #   support[0] += 0.01
+      #   support[1] -= 0.01
+      #
+      # width_support = support[1] - support[0]
       print(
         (f"dim: {dim:3d}, mode: {center:.4f}, {mode:.4f}, "
          f"support: [{support[0]:.4f}, {support[1]:.4f}], {width_support:.3f}, "
@@ -243,7 +181,7 @@ class Certificate:
 
   def find_k2(self, eps, k1, log_k):
 
-    sigma0, sigma1 = self.sigma0, self.sigma0
+    # sigma0, sigma1 = sigma0, self.sigma0
     is_close_below = lambda x, y: y - self.close_below_tol <= x <= y
 
     print('-- start loop --')
@@ -285,37 +223,39 @@ class Certificate:
 
 
   def compute_cert(self, eps):
+
     self.eps = eps
-    dim = self.dim
-    sigma0 = self.sigma0
-    sigma1 = self.sigma1
+    # sigma0 = self.sigma0
+    # sigma1 = self.sigma1
+    sigma0 = 0.25
+    sigma1 = 0.30
 
     make_noise = self.make_noise
     pABar_sigma0 = self.pABar_sigma0
     pABar_sigma1 = self.pABar_sigma1
 
-    k1 = self.compute_k1_cohen(eps)
-    log_k = self.compute_log_k(self.sigma0)
-    log_k2 = -1000000
+    k1 = self.compute_k1_cohen(eps, sigma0)
+    # log_k = self.compute_log_k(self.sigma0, dim)
+    log_k2 = -10000
 
     print(f'k1 = {k1:.4f}')
-    print(f'log_k = {log_k:.4f}')
+    # print(f'log_k = {log_k:.4f}')
     print()
 
-    noise = make_noise(self.sigma0, 2)
-    p1 = self.compute_set(noise, eps, k1, log_k2)
+    noise = make_noise(sigma0, 2)
+    p1 = self.compute_set(noise, eps, k1, log_k2, sigma0, sigma1)
     print(f'p1 (with k1 from compute set): {p1.cpu().numpy():.4f}')
 
-    noise = self.make_noise(self.sigma0, 2)
-    p1 = self.compute_from_integral(sigma0, eps, k1, log_k2)
+    # noise = self.make_noise(sigma0, 2)
+    # p1 = self.compute_from_integral(sigma0, eps, k1, log_k2)
     # print(f'p1 (with k1 from integral): {p1:.4f}')
     # print()
 
-    self.find_mode_and_width_of_bell_curve(sigma0, eps, k1, log_k2)
+    self.find_mode_and_width_of_bell_curve(eps, k1, log_k2, sigma0, sigma1)
 
 
-    # log_k = self.compute_log_k(self.sigma0)
-    # noise = self.make_noise(self.sigma0, 2)
+    # log_k = self.compute_log_k(sigma0)
+    # noise = self.make_noise(sigma0, 2)
     # g = self.compute_set(noise, eps, k1, log_k2, return_mean=False)
     # r = torch.abs(noise[:, 0]) * (g * 1.)
     # int1  = (r * np.exp(1/(dim-2) * log_k) )**(dim-2)
@@ -336,7 +276,7 @@ class Certificate:
     # print('k1', k1)
     # print('log_k2', log_k2)
 
-    # noise = make_noise(self.sigma0, 2)
+    # noise = make_noise(sigma0, 2)
     # p1 = self.compute_set(noise, 2, eps, k1_cohen, -10000, log_k)
     # print('p1', p1)
 
@@ -344,10 +284,10 @@ class Certificate:
     # k1, log_k2 = self.find_k_values(dim, eps, k1, K_sigma0, R)
     # print('log_k2', log_k2)
     
-    # p1 = self.compute_set(make_noise(self.sigma0, self.dim), eps, k1, log_k2)
-    # p2 = self.compute_set(make_noise(self.sigma1, self.dim), eps, k1, log_k2)
+    # p1 = self.compute_set(make_noise(sigma0, dim), eps, k1, log_k2)
+    # p2 = self.compute_set(make_noise(self.sigma1, dim), eps, k1, log_k2)
 
-    # noise = self.make_noise(self.sigma0, 2)
+    # noise = self.make_noise(sigma0, 2)
     # p1 = self.compute_from_integral(noise, eps, k1, log_k2, log_k) * (sigma0**2 / sigma1**2)
     # noise = self.make_noise(self.sigma1, 2)
     # p2 = self.compute_from_integral(noise, eps, k1, log_k2, log_k) * (sigma0**2 / sigma1**2)
@@ -366,7 +306,7 @@ class Certificate:
     
     eps = 0.25
     k1 = self.compute_k1_cohen(eps)
-    log_k = self.compute_log_k(self.sigma0)
+    log_k = self.compute_log_k(sigma0)
     log_k2 = -100000
     
     x0 = np.linspace(  0, 10, n_points)
@@ -376,7 +316,7 @@ class Certificate:
     x = np.hstack((grid[0].flatten().reshape(-1, 1), grid[1].flatten().reshape(-1, 1)))
     
     for i, dim in enumerate([20, 100, 120, 130, 150, 200, 300, 350]):
-      self.dim = dim
+
       Z = self.f(x, eps, k1, log_k2, log_k).reshape(n_points, n_points)
       ax = fig.add_subplot(2, 4, i+1, projection='3d')
       surf = ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=cm.coolwarm, linewidth=0, antialiased=False)
